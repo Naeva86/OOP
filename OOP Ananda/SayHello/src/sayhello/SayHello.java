a7771d689c7ac1856df4f1f33baf0ed82dd66aea
Ananda <127906104+Naeva86@users.noreply.github.com>
2024-03-25 09:04:31 +0800

    Add files via upload

---
+++ OOP Ananda/SayHello/src/sayhello/SayHello.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sayhello;
+
+/**
+ *
+ * @author INSTIKI
+ */
+public class SayHello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Halo Baaskara, apa kabar? ");
+        System.out.println("baik");
+        System.out.print("Selamat Belajar java");
+    }
+    
+}
